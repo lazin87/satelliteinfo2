7bb00441cf9324ad28b2beb69d06644d6e794c85
lazin87 <rafal.lazinski@gmail.com>
2015-07-22 15:46:40 +0200

    chttpbrowsersynchro completed

--- chttpbrowsersynchro.cpp
+++ chttpbrowsersynchro.cpp
@@ -1,9 +1,21 @@
 #include "chttpbrowsersynchro.h"
 
+#include <QTimer>
+
 CHttpBrowserSynchro::CHttpBrowserSynchro(QObject *a_pParent)
     : QObject(a_pParent),
       m_pNetworkProxy(NULL),
-      m_pReplay(NULL)
+      m_pReplay(NULL),
+      m_i64Pos(0),
+      m_oNetworkError(QNetworkReply::NoError),
+      m_i64BufferSize(0),
+      m_iOpenTimeoutMs(0),
+      m_iReadTimeoutMs(0),
+      m_i64Size(0),
+      m_iResponse(0),
+      m_fHaveSize(false),
+      m_fReadTimeout(false),
+      m_iReadFails(0)
 {
     m_pSocketThread = new QThread(this);
     moveToThread(m_pSocketThread);
@@ -33,6 +45,11 @@ void CHttpBrowserSynchro::close()
         m_pReplay->deleteLater();
         m_pReplay = NULL;
     }
+}
+
+QNetworkReply::NetworkError CHttpBrowserSynchro::error() const
+{
+    return m_oNetworkError;
 }
 
 bool CHttpBrowserSynchro::open(qint64 a_i64Offset)
@@ -79,7 +96,63 @@ bool CHttpBrowserSynchro::isGuiThread()
 
 void CHttpBrowserSynchro::resetReadFails()
 {
-    // TO DO
+    m_iReadFails = 0;
+}
+
+bool CHttpBrowserSynchro::waitForConnect(int a_iTimeOutms, QNetworkAccessManager *a_pManager)
+{
+    QTimer *pTimer = NULL;
+    QEventLoop oEventLoop;
+    bool fReadTimeout = false;
+
+    m_fReadTimeout = false;
+
+    if(0 < a_iTimeOutms)
+    {
+        pTimer = new QTimer(this);
+
+        connect( pTimer, SIGNAL(timeout() ),
+                 this, SLOT(slotWaitTimeout() )
+               );
+
+        pTimer->setSingleShot(true);
+        pTimer->start(a_iTimeOutms);
+
+        connect( this, SIGNAL(signalReadTimeout() ),
+                 &oEventLoop, SLOT(quit() )
+                );
+    }
+
+    // Wait on QnetworkManager reply here
+    connect( a_pManager, SIGNAL(finished(QNetworkReply*) ),
+             &oEventLoop, SLOT(quit())
+           );
+
+    if(NULL != m_pReplay)
+    {
+        connect( m_pReplay, SIGNAL(readyRead() ),
+                 &oEventLoop, SLOT(quit() )
+               );
+    }
+    oEventLoop.exec();
+
+    if(NULL != pTimer)
+    {
+        pTimer->stop();
+        delete pTimer;
+        pTimer = NULL;
+    }
+
+    fReadTimeout = m_fReadTimeout;
+    m_fReadTimeout = false;
+
+    return !fReadTimeout;
+}
+
+// returns response code (200, 404, etc)
+int CHttpBrowserSynchro::response() const
+{
+    return m_iResponse;
 }
 
 void CHttpBrowserSynchro::slotOpen(void *a_pReturnSuccess, void *a_pLoop, qint64 a_i64Offset)
@@ -117,20 +190,158 @@ bool CHttpBrowserSynchro::workerOpen(qint64 a_i64Offset)
     QNetworkRequest oRequest;
     oRequest.setUrl(m_oUrl);
     oRequest.setRawHeader("User-Agent", "Qt NetworkAccess 1.3");
+
+    m_i64Pos = a_i64Offset;
+    if(0 != m_i64Pos)
+    {
+        QByteArray data;
+        QString strData("bytes=" + QString::number(m_i64Pos) + "-");
+
+        data = strData.toLatin1();
+        oRequest.setRawHeader("Range", data);
+    }
+
+    m_pReplay = pManager->get(oRequest);
+
+    if(NULL == m_pReplay)
+    {
+        qDebug() << "CHttpBrowserSynchro::open(): network error";
+        m_oNetworkError = QNetworkReply::UnknownNetworkError;
+        return false;
+    }
+
+    m_pReplay->setReadBufferSize(m_i64BufferSize);
+    connect( m_pReplay, SIGNAL(error(QNetworkReply::NetworkError) ),
+             SLOT(slotError(QNetworkReply::NetworkError) )
+           );
+    connect( m_pReplay, SIGNAL(sslErrors(QList<QSslError>) ),
+             SLOT(slotSslErrors(QList<QSslError>) )
+           );
+
+    if(!waitForConnect(m_iOpenTimeoutMs, pManager) )
+    {
+        qDebug() << "CHttpBrowserSynchro::open(): timeout error";
+        m_oNetworkError = QNetworkReply::TimeoutError;
+        return false;
+    }
+
+    if(NULL == m_pReplay)
+    {
+        qDebug() << "CHttpBrowserSynchro::open(): cancelled";
+        m_oNetworkError = QNetworkReply::OperationCanceledError;
+        return false;
+    }
+
+    if( QNetworkReply::NoError != m_pReplay->error() )
+    {
+        qDebug() << "CHttpBrowserSynchro::open(): error" << m_pReplay->errorString();
+        m_oNetworkError = m_pReplay->error();
+        return false;
+    }
+
+    m_oNetworkError = m_pReplay->error();
+
+    m_strContentType = m_pReplay->header(QNetworkRequest::ContentTypeHeader).toString();
+    m_oLastModified = m_pReplay->header(QNetworkRequest::LastModifiedHeader).toDateTime();
+    m_i64Size = m_pReplay->header(QNetworkRequest::ContentLengthHeader).toULongLong();
+
+    m_iResponse = m_pReplay->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    m_strResponse = m_pReplay->attribute(QNetworkRequest::HttpReasonPhraseAttribute).toString();
+    m_fHaveSize = (0 != m_i64Size);
+
+    m_i64Size += m_i64Pos;
+
+    if(QNetworkReply::NoError != error() )
+    {
+        qDebug() << "CHttpBrowserSynchro::open(): error" << error();
+        return false;
+    }
+
+    m_oNetworkError = responseToError(m_iResponse);
+
+    qDebug() << "CHttpBrowserSynchro::open(): end response" << response()
+             << "error" << error() << "size" << m_i64Size;
+
+    return (200 == response() || 206 == response() );
 }
 
 void CHttpBrowserSynchro::clear()
 {
-    // TO DO
+    m_pReplay            = NULL;
+    //m_offset            = 0;
+    m_i64Size             = 0;
+    m_fHaveSize         = false;
+    m_i64Pos              = 0;
+    //m_nReceivedPos      = 0;
+    //m_nOverHead         = 0;
+    m_iReadFails        = 0;
+    m_oNetworkError      = QNetworkReply::NoError;
+    m_iResponse         = 0;
+    m_iOpenTimeoutMs    = DEF_OPENTIMEOUT;
+    m_iReadTimeoutMs    = DEF_READTIMEOUT;
+    m_fReadTimeout      = false;
+    m_strContentType.clear();
+    m_i64Size             = 0;
+    //m_sslerrors.clear();
+    m_i64BufferSize       = DEF_BUFFERSIZE;
+    //m_bResetMonitor     = false;
+}
+
+QNetworkReply::NetworkError CHttpBrowserSynchro::responseToError(int a_iResponse) const
+{
+    QNetworkReply::NetworkError error = QNetworkReply::NoError;
+
+    switch (a_iResponse)
+    {
+    case 200:
+    case 206:
+        break;
+    case 401:
+        error = QNetworkReply::AuthenticationRequiredError;
+        break;
+    case 403:
+        error = QNetworkReply::ContentOperationNotPermittedError;
+        break;
+    case 404:
+        error = QNetworkReply::ContentNotFoundError;
+        break;
+    default:
+        error = QNetworkReply::UnknownNetworkError;
+        break;
+    }
+
+    return error;
 }
 
 void CHttpBrowserSynchro::slotProxyAuthenticationRequired(const QNetworkProxy &proxy, QAuthenticator *authenticator)
 {
-    // TO DO
+    Q_UNUSED(proxy)
+    Q_UNUSED(authenticator)
+
+    qDebug() << "CHttpBrowserSynchro::slotProxyAuthenticationRequired()";
 }
 
 void CHttpBrowserSynchro::slotAuthenticationRequired(QNetworkReply *reply, QAuthenticator *authenticator)
 {
-    // TO DO
+    Q_UNUSED(reply)
+    Q_UNUSED(authenticator)
+
+    qDebug() << "CHttpBrowserSynchro::slotAuthenticationRequired()";
+}
+
+void CHttpBrowserSynchro::slotError(QNetworkReply::NetworkError)
+{
+    qDebug() << "CHttpBrowserSynchro::slotError()";
+}
+
+void CHttpBrowserSynchro::slotSslErrors(QList<QSslError>)
+{
+    qDebug() << "CHttpBrowserSynchro::slotSslErrors()";
+}
+
+void CHttpBrowserSynchro::slotWaitTimeout()
+{
+    m_fReadTimeout = true;  // Report timeout
+    emit signalReadTimeout();
 }
 
--- chttpbrowsersynchro.h
+++ chttpbrowsersynchro.h
@@ -9,6 +9,10 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+#define DEF_OPENTIMEOUT     8000 /* ms */
+#define DEF_READTIMEOUT     8000 /* ms */
+#define DEF_BUFFERSIZE      (1024*1204) /* = 1 MB */
+
 class CHttpBrowserSynchro : public QObject
 {
     Q_OBJECT
@@ -19,10 +23,13 @@ public:
 
     void endSocketThread();
     virtual void close();
+    QNetworkReply::NetworkError error() const;
 
     bool open(qint64 a_i64Offset = 0);
     bool isGuiThread();
     void resetReadFails();
+    bool waitForConnect(int a_iTimeOutms, QNetworkAccessManager *a_pManager);
+    int response() const;
 
 public slots:
     void slotOpen(void *a_pReturnSuccess, void *a_pLoop, qint64 a_i64Offset = 0);
@@ -30,6 +37,7 @@ public slots:
 protected:
     bool workerOpen(qint64 a_i64Offset = 0);
     void clear();
+    QNetworkReply::NetworkError responseToError(int a_iResponse) const;
 
 protected:
     const QNetworkProxy * m_pNetworkProxy;
@@ -37,10 +45,29 @@ protected:
     QEventLoop m_oEventLoop;
     QNetworkReply * m_pReplay;
     QUrl m_oUrl;
+    qint64 m_i64Pos;
+    QNetworkReply::NetworkError m_oNetworkError;
+    qint64 m_i64BufferSize;
+    int m_iOpenTimeoutMs;
+    int m_iReadTimeoutMs;
+    qint64 m_i64Size;
+    int m_iResponse;
+    bool m_fHaveSize;
+    bool m_fReadTimeout;
+    int m_iReadFails;
+    QString m_strResponse;
+    QString m_strContentType;
+    QDateTime m_oLastModified;
 
 private slots:
     void slotProxyAuthenticationRequired(const QNetworkProxy & proxy, QAuthenticator * authenticator);
     void slotAuthenticationRequired(QNetworkReply * reply, QAuthenticator * authenticator);
+    void slotError(QNetworkReply::NetworkError);
+    void slotSslErrors(QList<QSslError>);
+    void slotWaitTimeout();
+
+signals:
+    void signalReadTimeout();
 };
 
 #endif // CHTTPBROWSERSYNCHRO_H
