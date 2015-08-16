299a41d98a2183a9b6f076fe1d3dd6f3bbcd1901
Rafal87 <rafal.lazinski@gmail.com>
2015-08-16 18:04:22 +0200

    cpositionloger implement 16082015

--- cpositionloger.cpp
+++ cpositionloger.cpp
@@ -1,12 +1,63 @@
 #include "cpositionloger.h"
+#include <QDebug>
 
-CPositionLoger::CPositionLoger()
+#include "cgpspositionprovider.h"
+#include "cgpsdatastorage.h"
+#include "chttpdatastorage.h"
+
+CPositionLoger::CPositionLoger(QObject * a_pParent = 0)
+    : m_pGpsPosProvider(0)
+    , m_pLocalDataStorage(0)
+    , m_pRemoteDataStorage(0)
 {
+    initMembers();
 
+    if(0 != m_pGpsPosProvider)
+    {
+        connect(m_pGpsPosProvider, SIGNAL(positionChanged() ),
+                this, SLOT(newPositionDataReady() ) );
+    }
+    else
+    {
+        qCritical() << "GPS position provider is not available";
+    }
 }
 
 CPositionLoger::~CPositionLoger()
 {
+    deleteMembers();
+}
 
+void CPositionLoger::newPositionDataReady()
+{
+    // TO DO
+}
+
+void CPositionLoger::initMembers()
+{
+    m_pGpsPosProvider = new CGpsPositionProvider(this);
+    m_pLocalDataStorage = new CGpsDataStorage(this);
+    m_pRemoteDataStorage = new CHttpDataStorage(m_oHttpBrowser);
+}
+
+void CPositionLoger::deleteMembers()
+{
+    if(0 != m_pGpsPosProvider)
+    {
+        delete m_pGpsPosProvider;
+        m_pGpsPosProvider = 0;
+    }
+
+    if(0 != m_pLocalDataStorage)
+    {
+        delete m_pLocalDataStorage;
+        m_pLocalDataStorage = 0;
+    }
+
+    if(0 != m_pRemoteDataStorage)
+    {
+        delete m_pRemoteDataStorage;
+        m_pRemoteDataStorage = 0;
+    }
 }
 
--- cpositionloger.h
+++ cpositionloger.h
@@ -1,21 +1,33 @@
 #ifndef CPOSITIONLOGER_H
 #define CPOSITIONLOGER_H
 
-#include "cgpspositionprovider.h"
+#include <QObject>
+#include <chttpbrowsersync.h>
 
 class ILocalDataStorage;
 class IRemoteDataStorage;
+class CGpsPositionProvider;
 
-class CPositionLoger
+class CPositionLoger : public QObject
 {
+    Q_OBJECT
+
 public:
-    CPositionLoger();
+    explicit CPositionLoger(QObject * a_pParent = 0);
     ~CPositionLoger();
 
+private slots:
+    void newPositionDataReady();
+
 private:
-    CGpsPositionProvider m_oGpsPosProvider;
+    void initMembers();
+    void deleteMembers();
+
+    CGpsPositionProvider * m_pGpsPosProvider;
     ILocalDataStorage * m_pLocalDataStorage;
     IRemoteDataStorage * m_pRemoteDataStorage;
+
+    CHttpBrowserSync m_oHttpBrowser;
 };
 
 #endif // CPOSITIONLOGER_H
